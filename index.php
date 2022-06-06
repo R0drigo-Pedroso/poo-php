59b4a3bd480c71066353ab47015ed85d9e384ef6
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-06-06 11:59:35 -0300

    Uso do construtor
    Aplicando o método construtor

--- index.php
+++ index.php
@@ -4,33 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Exemplo 01 - PHP - POO</title>
+    <title>Exemplo 02 - PHP - POO</title>
 </head>
 <body>
-    <h1>PHP POO - Exemplo 01</h1>
+    <h1>PHP POO - Exemplo 02</h1>
     <hr>
 
     <ul>
         <li>
-            Criação da Classe
-        </li>
-
-        <li>
-            Importação do arquivo de Classe
-        </li>
-
-        <li>
             Criação dos Objetos
+        </li>
+
+        <li>
+            Uso do método construtor
+        </li>
+
+        <li>
+            Uso do <code>$this</code> para acessar as propriedades
         </li>
     </ul>
 
 <?php
     // Importando a Classe
-    require_once 'src/Clientes.php';
+    require_once 'src/Cliente.php';
 
     //Criação dos Objetos
-    $clienteA = new Cliente;
-    $clienteB = new Cliente;
+    $clienteA = new Cliente('Rodrigo', 'rodrigo@email.com');
+    $clienteB = new Cliente('Carla', 'carla@email.com');
 ?>
 
 <pre><?=var_dump($clienteA, $clienteB)?></pre>
--- src/Cliente.php
+++ src/Cliente.php
@@ -6,5 +6,13 @@ class Cliente {
     public $email; //string
     public $senha; //string
     public $telefones; //array
+
+    //Método construtor
+    public function __construct($nome, $email) {
+
+        //$this é uma variável especial, sempre apontando para a referência da Classe atualmente manipulada.
+        $this->nome = $nome;
+        $this->email = $email;        
+    }
 }
 

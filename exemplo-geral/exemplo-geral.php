2051f24089eaa67beaf8db5f0e01e08ef458b9aa
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-06-06 11:11:20 -0300

    Create exemplo-geral.php

---
+++ exemplo-geral/exemplo-geral.php
@@ -0,0 +1,17 @@
+<?php
+// exemplo-geral.php
+
+class Exemplo {
+    // Atributos ou Propriedade
+    public string $nome;
+    public string $escola;
+}
+
+
+// Criar Objetos/instâncias
+
+$exemploA = new Exemplo;
+
+echo "<pre>";
+    var_dump($exemploA);
+echo "</pre>";

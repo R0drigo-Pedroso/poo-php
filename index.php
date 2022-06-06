7c6af4855bea01a6b86aa0a350bfc1a88267d9c2
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-06-06 11:37:17 -0300

    Exemplo 01
    Criação de classe e objetos

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Exemplo 01 - PHP - POO</title>
+</head>
+<body>
+    <h1>PHP POO - Exemplo 01</h1>
+    <hr>
+
+    <ul>
+        <li>
+            Criação da Classe
+        </li>
+
+        <li>
+            Importação do arquivo de Classe
+        </li>
+
+        <li>
+            Criação dos Objetos
+        </li>
+    </ul>
+
+<?php
+    // Importando a Classe
+    require_once 'src/Clientes.php';
+
+    //Criação dos Objetos
+    $clienteA = new Cliente;
+    $clienteB = new Cliente;
+?>
+
+<pre><?=var_dump($clienteA, $clienteB)?></pre>
+
+</body>
+</html>
---
+++ src/Cliente.php
@@ -0,0 +1,10 @@
+<?php
+
+class Cliente {
+    // Propriedade (ou atributos)
+    public $nome; //string
+    public $email; //string
+    public $senha; //string
+    public $telefones; //array
+}
+
